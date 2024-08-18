bc8bd341c6d20b3f9d18dc5e5fa5014b3a202f54
Deepak <notwho53@gmail.com>
2024-08-18 23:32:23 +0530

    living on the edge

--- bin/src/build-tools/common.sh
+++ bin/src/build-tools/common.sh
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-export DROIDIAN_RELEASE="${DROIDIAN_RELEASE:-bookworm}"
+export DROIDIAN_RELEASE="${DROIDIAN_RELEASE:-next}"
 
 info() {
 	echo "I: $@"
